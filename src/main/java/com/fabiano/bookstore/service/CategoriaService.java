e2b06542fc7312640c3913edb3588d88208507e0
Fabiano Garcia <fabianojp06@gmail.com>
2022-10-21 10:48:41 -0300

    Update para categoria

--- src/main/java/com/fabiano/bookstore/service/CategoriaService.java
+++ src/main/java/com/fabiano/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fabiano.bookstore.domain.Categoria;
+import com.fabiano.bookstore.dtos.CategoriaDTO;
 import com.fabiano.bookstore.repositories.CategoriaRepository;
 import com.fabiano.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -30,5 +31,13 @@ public class CategoriaService {
 		obj.setId(null);
 		return repository.save(obj);
 	}
+	
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
+		return repository.save(obj);
+	}
+
 
 }
