a6d1eac536930c73a8cc4e9b63d563fdc14a5af8
Fabiano Garcia <fabianojp06@gmail.com>
2022-10-24 11:13:54 -0300

    Exceção personalizada para DELETE de categoria

--- src/main/java/com/fabiano/bookstore/service/CategoriaService.java
+++ src/main/java/com/fabiano/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.fabiano.bookstore.domain.Categoria;
@@ -22,27 +23,31 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id:" + id + " , Tipo" + Categoria.class.getName(), null));
 	}
-	
+
 	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
 	}
-	
+
 	public Categoria update(Integer id, CategoriaDTO objDto) {
 		Categoria obj = findById(id);
 		obj.setNome(objDto.getNome());
 		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		findById(id);
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.fabiano.bookstore.service.exceptions.DataIntegrityViolationException(
+					"A Categoria não pode ser excluída! Possui livros associado.");
+		}
 	}
-
 
 }
