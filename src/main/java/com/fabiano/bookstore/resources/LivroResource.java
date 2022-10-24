00df1eed6e18efa9f1de87ccee39cc85e428d25a
Fabiano Garcia <fabianojp06@gmail.com>
2022-10-24 12:19:57 -0300

    Implementação método GETBYID livors

--- src/main/java/com/fabiano/bookstore/resources/LivroResource.java
+++ src/main/java/com/fabiano/bookstore/resources/LivroResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,14 @@ public class LivroResource {
 
 	@Autowired
 	private LivroService service;
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Livro> findById(@PathVariable Integer id) {
+		Livro obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	
 	@GetMapping
 	public ResponseEntity<List<LivroDTO>> findAll() {
--- src/main/java/com/fabiano/bookstore/service/LivroService.java
+++ src/main/java/com/fabiano/bookstore/service/LivroService.java
@@ -1,19 +1,28 @@
 package com.fabiano.bookstore.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fabiano.bookstore.domain.Categoria;
 import com.fabiano.bookstore.domain.Livro;
 import com.fabiano.bookstore.repositories.LivroRepository;
+import com.fabiano.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class LivroService {
-	
+
 	@Autowired
 	private LivroRepository repository;
-	
+
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:" + id + " , Tipo" + Categoria.class.getName(), null));
+	}
+
 	public List<Livro> findAll() {
 		return repository.findAll();
 	}
