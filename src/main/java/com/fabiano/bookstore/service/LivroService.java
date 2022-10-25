777506fe9d57a6c5598364619365335259cc9758
Fabiano Garcia <fabianojp06@gmail.com>
2022-10-25 08:26:50 -0300

    Atualizaçaõ PATCH e PUT livro

--- src/main/java/com/fabiano/bookstore/service/LivroService.java
+++ src/main/java/com/fabiano/bookstore/service/LivroService.java
@@ -27,4 +27,18 @@ public class LivroService {
 		return repository.findAll();
 	}
 
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());
+	}
+	
+	
+
 }
